860652e56cec666c0760c2c476e2a0c3cf73cf07
Maxim Kazakov <kazakov.java@icloud.com>
2023-12-25 09:29:14 +0500

    Отправил Олегу, у меня не запускается

--- src/test/java/JUnitExamples.java
+++ src/test/java/JUnitExamples.java
@@ -2,31 +2,34 @@ import org.junit.jupiter.api.*;
 
 public class JUnitExamples {
     @BeforeAll //выполняется перед всеми тестами
-    static void beforeAll(){
+    static void beforeAll() {
         System.out.println("Here is beforeAll()");
     }
 
-    @BeforeEach // выполняется перед каждым тестом
-    void beforeEach(){
+    @BeforeEach
+        // выполняется перед каждым тестом
+    void beforeEach() {
         System.out.println("    Here is beforeEach()");
     }
 
     @AfterAll  //выполняется после всех тестов
-    static void afterAll(){
+    static void afterAll() {
         System.out.println("Here is afterAll()");
     }
 
-    @AfterEach // выполняется после каждого теста
-    void afterEach(){
+    @AfterEach
+        // выполняется после каждого теста
+    void afterEach() {
         System.out.println("        Here is afterEach()");
     }
+
     @Test
-    void firstTest(){
+    void firstTest() {
         System.out.println("        Here is firstTest()");
     }
 
     @Test
-    void secondTest(){
+    void secondTest() {
         System.out.println("        Here is secondTest()");
     }
 }
---
+++ src/test/java/TextBoxTest.java
@@ -0,0 +1,29 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TextBoxTest {
+    @BeforeAll
+    static void beforeAll(){
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+    }
+
+    @Test
+    void fillFormTest() {
+        open("/text-box");
+        $("#userName").setValue("Maxim Kazakov");
+        $("#userEmail").setValue("kazakov.java@icloud.com");
+        $("#currentAddress").setValue("Some address 1");
+        $("#permanentAddress").setValue("Another address");
+        $("#submit").click();
+        $("#output").shouldHave(text("Maxim Kazakov"), text("kazakov.java@icloud.com"),
+                text("Some address 1"), text("Another address"));
+
+
+    }
+}
